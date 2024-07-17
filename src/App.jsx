7c377a063647dcd1212eb891dd039f30430c3aff
Nikolas <nikolasrailanmp@gmail.com>
2024-07-17 10:31:08 -0300

    router produtos e contatos. Secao contato animada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Produtos from "./Components/Produtos";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Contato from "./Components/Contato";
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
